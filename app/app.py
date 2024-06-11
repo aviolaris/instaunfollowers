c3a2375791cc9899fce8d5ac33264fd8b43dca77
Andreas Violaris <48277853+aviolaris@users.noreply.github.com>
2024-06-11 23:48:34 +0300

    Bump instaunfollowers from 1.11.17 to 1.11.18

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:
 # Upload folder
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 # Current version
-CURRENT_VERSION = 'v1.11.17'
+CURRENT_VERSION = 'v1.11.18'
 # Update needed
 UPDATE_NEEDED = bool(update_needed(CURRENT_VERSION, get_latest_version()))
 
